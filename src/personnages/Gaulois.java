64fc4d664a1688eadf2e4a9f6c090797976535a6
PLP4296A <PLP4296A@u2-209-09.ens.fsi>
2022-09-27 19:51:27 +0200

    ILU1-TP1-LesGaulois

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -1,5 +1,35 @@
 package personnages;
 
 public class Gaulois {
+	private String nom;
+	private int force;
+	private int effetPotion = 1;
+
+	public Gaulois(String nom, int force) {
+		super();
+		this.nom = nom;
+		this.force = force;
+	}
+
+	public int getForce() {
+		return force;
+	}
+
+	public void setForce(int force) {
+		this.force = force;
+	}
+
+	public int getEffetPotion() {
+		return effetPotion;
+	}
+
+	@Override
+	public String toString() {
+		return "Gaulois [nom=" + nom + ", force=" + force + ", effetPotion=" + effetPotion + "]";
+	}
+
+	public static void main(String[] args) {
+
+	}
 
 }
