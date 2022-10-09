96a69e2d2e56f218fd19ee84640c1852c090ef33
Xelvo <114509450+Xelvo@users.noreply.github.com>
2022-10-09 18:31:56 +0200

    Update Gaulois.java

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -181,3 +181,10 @@ public class Village {
 
 /* NEW PACKAGE : histoire */
 package histoire;
+
+public class Scenario {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+}
