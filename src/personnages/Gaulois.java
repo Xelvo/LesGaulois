efd47126a9854f1e48be1c2eac6c368a3b07d312
Xelvo <114509450+Xelvo@users.noreply.github.com>
2022-10-09 18:28:15 +0200

    Update Gaulois.java

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -1,18 +1,16 @@
 package personnages;
 
-
-/*
 public class Gaulois {
 	private String nom;
 	private int force;
 	private int effetPotion = 1;
-
+	
 	public Gaulois(String nom, int force) {
 		super();
 		this.nom = nom;
 		this.force = force;
 	}
-
+	
 	public int getForce() {
 		return force;
 	}
@@ -20,39 +18,23 @@ public class Gaulois {
 	public void setForce(int force) {
 		this.force = force;
 	}
-
+	
 	public int getEffetPotion() {
 		return effetPotion;
 	}
-
-	@Override
-	public String toString() {
-		return "Gaulois [nom=" + nom + ", force=" + force + ", effetPotion=" + effetPotion + "]";
-	}
-
-	public static void main(String[] args) {
-
-	}	
-}
-*/
-
-
-public class Gaulois {
-	private String nom;
-	private int force;
 	
-	public Gaulois(String nom, int force) {
-		this.nom = nom;
-		this.force = force;
-	}
 	public String getNom() {
 		return nom;
 	}
+	
 	public void parler(String texte) {
 		System.out.println(prendreParole() + "« " + texte + "»");
 	}
+	
 	private String prendreParole() {
-		return "Le gaulois " + nom + " : ";
+		String coucou = "Le gaulois " + nom + " : ";
+		System.out.print(coucou);
+		return coucou
 	}
 	
 	public void frapper(Romain romain) {
@@ -63,17 +45,18 @@ public class Gaulois {
 	@Override
 	public String toString() {
 		return "Gaulois [nom=" + nom + ", force=" + force + ", effetPotion=" + effetPotion + "]";
-		System.out.println(asterix);
 	}
 	
 	public static void main(String[] args) {
-		//TODO créer un main permettant de tester la classe Gaulois
-		asterix (nom = "Astérix", force = 8);
-		/*System.out.println(asterix);*/
-		System.out.println(nom);
+		Gaulois asterix = new Gaulois("Astérix", 8);
+		System.out.println(asterix);
+		asterix.prendreParole();
 	}
 }
 
+
+/* METTRE DANS UN NEW CLASS DANS PACKAGE */
+package personnages;
 
 public class Romain {
 	private String nom;
@@ -107,15 +90,20 @@ public class Romain {
 }
 
 
+/* METTRE DANS UN NEW CLASS DANS PACKAGE */
+package personnages;
+
 public class Druide {
 	private String nom;
 	private int effetPotionMin;
 	private int effetPotionMax;
+	private int forcePotion = 1;
 	
 	public Druide(String nom, int effetPotionMin, int effetPotionMax) {
 		this.nom = nom;
 		this.effetPotionMin = effetPotionMin;
 		this.effetPotionMax = effetPotionMax;
+		this.forcePotion = forcePotion;
 		parler("Bonjour, je suis le druide " + nom + " et ma potion peut aller d'une force " + effetPotionMin + " à " + effetPotionMax + ".");
 	}
 	
@@ -126,11 +114,19 @@ public class Druide {
 	public void parler(String texte) {
 		System.out.println(prendreParole() + "« " + texte + "»");
 	}
+	
 	private String prendreParole() {
 		return "Le druide " + nom + " : ";
 	}
+	
+	public static void preparerPortion() {
+	}	
 }
 
+
+
+/* METTRE DANS UN NEW CLASS DANS PACKAGE */
+package personnages;
 
 public class Chef {
 	private String nom;
@@ -152,6 +148,7 @@ public class Chef {
 	public void parler(String texte) {
 		System.out.println(prendreParole() + "« " + texte + "»");
 	}
+	
 	private String prendreParole() {
 		return "Le chef " + nom + " du village " + village.getNom() + " : ";
 	}
@@ -161,3 +158,26 @@ public class Chef {
 		romain.recevoirCoup(force / 3);
 	}
 }
+
+/* METTRE DANS UN NEW CLASS DANS PACKAGE */
+package personnages;
+
+public class Village {
+	private String nom;
+	private Chef chef;
+	
+	public Village(String nom) {
+		this.nom = nom;
+	}
+	
+	public void setChef(Chef chef) {
+		this.chef = chef;
+	}
+	
+	public String getNom() {
+		return nom;
+	}
+}
+
+/* NEW PACKAGE : histoire */
+package histoire;
