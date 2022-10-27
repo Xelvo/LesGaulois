9d8decec3bd5ca7023feb682956a3f0f844e34b5
Xelvo <114509450+Xelvo@users.noreply.github.com>
2022-10-27 10:30:37 +0200

    Update Gaulois.java

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -1,6 +1,5 @@
 package personnages;
 
-
 public class Gaulois {
 	private String nom;
 	private int force, nb_trophees;
@@ -30,7 +29,7 @@ public class Gaulois {
 	}
 
 	public void parler(String texte) {
-		System.out.println(prendreParole() + "« " + texte + "»");
+		System.out.println(prendreParole() + "Â« " + texte + "Â»");
 	}
 
 //	private String prendreParole() {
@@ -39,22 +38,34 @@ public class Gaulois {
 //		return coucou;
 //		
 //	}
+	
+	
+	private String prendreParole() {
+		return "Le gaulois " + nom + " : ";
+
+		
+	}
 
 //	public void frapper(Romain romain) {
-//		System.out.println(nom + " envoie un grand coup dans la mâchoire de " + romain.getNom());
-//		romain.recevoirCoup(force / 3);
+//		System.out.println(nom + " envoie un grand coup dans la mÃ¢choire de " + romain.getNom());
+//		romain.recevoirCoup((force / 3) * effetPotion);
 //	}
 
-//	@Override
-//	public String toString() {
-//		return "Gaulois [nom=" + nom + ", force=" + force + ", effetPotion=" + effetPotion + "]";
-//		
-//	}
-//	
+	@Override
+	public String toString() {
+		return "Gaulois [nom=" + nom + ", force=" + force + ", effetPotion=" + effetPotion + "]";
+		
+	}
+	
 	public static void main(String[] args) {
-		Gaulois asterix = new Gaulois("Astérix", 8);
+		Gaulois asterix = new Gaulois("AstÃ©rix", 8);
+		Romain minus = new Romain("Minus", 6);
+		Druide panoramix = new Druide("Panoramix",5,10);
 		System.out.println(asterix);
 		asterix.prendreParole();
+		asterix.frapper(minus);
+		panoramix.preparerPotion();
+		asterix.boirePotion(panoramix.effetPotionPreparee);
 
 	}
 	
@@ -64,12 +75,16 @@ public class Gaulois {
 	}
 	
 	public void frapper(Romain romain) {
-		System.out.println(nom + " envoie un grand coup dans la mâchoire de " + romain.getNom());
-		Equipement trophees[] = romain.recevoirCoup((force / 3) * effetPotion);
+		System.out.println(nom + " envoie un grand coup dans la mÃ¢choire de " + romain.getNom());
+		trophees = romain.recevoirCoup((force / 3) * effetPotion);
 		for (int i = 0; trophees != null && i < trophees.length; i++, nb_trophees++) {
 			this.trophees[nb_trophees] = trophees[i];
 		}
-		return;
+	}
+	
+	public void boirePotion(int effetPotionPreparee) {
+		effetPotion = effetPotionPreparee;
+		parler("Merci Druide, je sens ma force est"+effetPotion + " fois dÃ©cuplÃ©");
 	}
 	
 }
