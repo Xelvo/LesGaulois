4233f6702225a87d3ab16dad783989585525144a
Xelvo <114509450+Xelvo@users.noreply.github.com>
2022-10-27 10:09:41 +0200

    Update Druide.java

--- src/personnages/Druide.java
+++ src/personnages/Druide.java
@@ -1,18 +1,17 @@
 package personnages;
+import java.util.Random;
 
 public class Druide {
 	private String nom;
 	private int effetPotionMin;
 	private int effetPotionMax;
-	private int forcePotion = 1;
+	private int effetPotionPreparee = 1;
 
-	public Druide(String nom, int effetPotionMin, int effetPotionMax, int forcePotion) {
+	public Druide(String nom, int effetPotionMin, int effetPotionMax) {
 		this.nom = nom;
 		this.effetPotionMin = effetPotionMin;
 		this.effetPotionMax = effetPotionMax;
-		this.forcePotion = forcePotion;
-		parler("Bonjour, je suis le druide " + nom + " et ma potion peut aller d'une force " + effetPotionMin + " � "
-				+ effetPotionMax + ".");
+		parler("Bonjour, je suis le druide " + nom + " et ma potion peut aller d'une force " + effetPotionMin + " à " + effetPotionMax + ".");
 	}
 
 	public String getNom() {
@@ -20,15 +19,37 @@ public class Druide {
 	}
 
 	public void parler(String texte) {
-		System.out.println(prendreParole() + "� " + texte + "�");
+		System.out.println(prendreParole() + "« " + texte + "»");
 	}
 
 	private String prendreParole() {
 		return "Le druide " + nom + " : ";
 	}
 	
-	public static void preparerPortion() {
-		
+	public void preparerPortion() {
+		Random rand = new Random();
+		effetPotionPreparee = rand.nextInt(effetPotionMax - effetPotionMin + 1);
+		effetPotionPreparee += effetPotionMin;
+		if (effetPotionPreparee > 7) {
+			parler("J'ai preparée une super potion de force : " + effetPotionPreparee);
+		}
+		else {
+			parler("Je n'ai pas trouvé tous les ingrédients, ma potion est seulement de force : " + effetPotionPreparee);
+		}
 	}
 	
+	public void booster(Gaulois gaulois) {
+		String nomGaulois = gaulois.getNom();
+		if ("Obelix".equals(nomGaulois)) {
+			parler("Non, " + nomGaulois + "!... Tu n'auras pas de potion magique");
+		}
+		else {
+			gaulois.boirePotion(effetPotionPreparee);
+		}		
+	}
+	
+	public stactic void main(String[] args) {
+		Druide panoramix = new Druide("Panoramix",5,10);
+		panoramix.preparerPotion();
+	}
 }
